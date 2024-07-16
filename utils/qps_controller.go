79531e7d74b4377e280f97e23035312e334deba1
will@2012 <xibaow2020@qq.com>
2024-07-16 22:04:46 +0800

    chore: debug qps controller

--- utils/qps_controller.go
+++ utils/qps_controller.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"time"
+	"context"
 
 	"golang.org/x/time/rate"
 )
@@ -18,13 +18,13 @@ func (c *QPSController) Init(qps uint64) {
 		return
 	}
 	// 1 second = 1000000us
-	limit := rate.Every(time.Duration(1000000.0/qps) * time.Microsecond)
-	c.tokenBucket = rate.NewLimiter(limit, 100)
+	//limit := rate.Every(time.Duration(1000000.0/qps) * time.Microsecond)
+	c.tokenBucket = rate.NewLimiter(rate.Limit(qps), 100)
 }
 
 func (c *QPSController) TakeToken() {
 	if c == nil {
 		return
 	}
-	c.tokenBucket.Wait(nil)
+	c.tokenBucket.Wait(context.Background())
 }
